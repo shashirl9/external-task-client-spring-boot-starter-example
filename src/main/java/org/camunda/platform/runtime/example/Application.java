cd7e67ffa824dc7c5ff954d6a1f5c6bee13c01b5
shashirl9 <shashidhar.lucky9@gmail.com>
2024-03-03 14:57:15 +1100

    request rejector

--- src/main/java/org/camunda/platform/runtime/example/Application.java
+++ src/main/java/org/camunda/platform/runtime/example/Application.java
@@ -16,14 +16,50 @@
  */
 package org.camunda.platform.runtime.example;
 
+import org.camunda.bpm.client.ExternalTaskClient;
+import org.camunda.bpm.client.topic.TopicSubscriptionBuilder;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
 
-  public static void main(String... args) {
-    SpringApplication.run(Application.class, args);
-  }
+    public static void main(String... args) {
+        SpringApplication.run(Application.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        ExternalTaskClient client = ExternalTaskClient.create()
+                .baseUrl("http://localhost:9991/engine-rest")
+                .asyncResponseTimeout(20000)
+                .lockDuration(10000)
+                .maxTasks(1)
+                .build();
+
+        TopicSubscriptionBuilder subscriptionBuilder = client
+                .subscribe("requestRejecter");
+
+        // handle job
+        subscriptionBuilder.handler((externalTask, externalTaskService) -> {
+            String customerId = externalTask.getVariable("customerId");
+            int creditScore = externalTask.getVariable("creditScore");
+            Logger.getLogger("requestRejecter")
+                    .log(Level.INFO, "Sorry, your loan request for the CustomerId: {0} with Credit-score: {1} has been rejected!", new Object[]{customerId, creditScore});
+            Map<String, Object> variables = new HashMap<String, Object>();
+            variables.put("Notfication - Timestamp", new Date());
+            externalTaskService.complete(externalTask, variables);
+
+        });
+
+        subscriptionBuilder.open();
+    }
 }
